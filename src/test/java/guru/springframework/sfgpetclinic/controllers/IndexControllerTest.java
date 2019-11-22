c4090f862c6547da722b32009a8ac7544e58a4cd
Octavio Marchi Parisi <octaviomarchi@msn.com>
2019-11-22 15:25:10 -0800

    51. JUnit Test Display Names

--- src/test/java/guru/springframework/sfgpetclinic/controllers/IndexControllerTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/controllers/IndexControllerTest.java
@@ -1,30 +1,37 @@
 package guru.springframework.sfgpetclinic.controllers;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class IndexControllerTest {
 
 	IndexController controller;
 
-	@BeforeEach
-	void setUp() {
-		controller = new IndexController();
-	}
+    @BeforeEach
+    void setUp() {
+        controller = new IndexController();
+    }
 
-	@Test
-	void testIndex() {
-		assertEquals("index", controller.index());
-		assertEquals("index", controller.index(), "Wrong View Returned");
-		assertEquals("index", controller.index(), () -> "Another Expensive Message " + "Make me only if you have to");
-	}
+    @DisplayName("Test Proper View name is returned for index page")
+    @Test
+    void index() {
+        assertEquals("index", controller.index());
+        assertEquals("index", controller.index(), "Wrong View Returned");
 
-	@Test
-	void testOupsHandler() {
-		assertTrue("notimplemented".equals(controller.oupsHandler()),
-				() -> "This is some expensive " + "Message to build" + "for my test");
-	}
+        assertEquals("index", controller.index(), () -> "Another Expensive Message " +
+                "Make me only if you have to");
+    }
+
+    @Test
+    @DisplayName("Test exception")
+    void oupsHandler() {
+        assertTrue("notimplemented".equals(controller.oupsHandler()), () -> "This is some expensive " +
+                "Message to build" +
+                "for my test");
+    }
 
 }
