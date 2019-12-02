43998c7b02f680e7ddfd18337fe5c2681768954a
Octavio Marchi Parisi <octaviomarchi@msn.com>
2019-12-02 15:57:58 -0800

    56. Using AssertJ with JUnit

--- src/test/java/guru/springframework/sfgpetclinic/controllers/IndexControllerTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/controllers/IndexControllerTest.java
@@ -1,5 +1,6 @@
 package guru.springframework.sfgpetclinic.controllers;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTimeout;
@@ -34,6 +35,8 @@ class IndexControllerTest {
 		assertEquals("index", controller.index(), "Wrong View Returned");
 
 		assertEquals("index", controller.index(), () -> "Another Expensive Message " + "Make me only if you have to");
+		
+		assertThat(controller.index()).isEqualTo("index");
 	}
 
 	@Test
