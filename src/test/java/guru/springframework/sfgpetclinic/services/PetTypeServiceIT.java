9e0b1f12ae9d2e86f8d6bc763d8d43c7cfc93182
Octavio Marchi Parisi <octaviomarchi@msn.com>
2020-01-08 10:55:05 -0800

    77. Unit Tests vs Integration Tests

---
+++ src/test/java/guru/springframework/sfgpetclinic/services/PetTypeServiceIT.java
@@ -0,0 +1,32 @@
+package guru.springframework.sfgpetclinic.services;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class PetTypeServiceIT {
+
+	@BeforeEach
+	void setUp() {
+	}
+
+	@Test
+	void testFindAll() {
+	}
+
+	@Test
+	void testFindById() {
+	}
+
+	@Test
+	void testSave() {
+	}
+
+	@Test
+	void testDelete() {
+	}
+
+	@Test
+	void testDeleteById() {
+	}
+
+}
